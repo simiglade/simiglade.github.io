6e640a9f5f7bf1526df841752ce09eb01aebb90f
silva <sylvernus@glade.ng>
2021-07-14 09:22:31 +0100

    update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -103,18 +103,34 @@ function Home(props) {
             A business current account that is packed with features which save
             you time and money.
           </SectionInfoDescription>
-          <SectionInfoTitle>Business current account</SectionInfoTitle>
+
+          <SectionInfoTitle>
+            <span class="material-icons md-18 mr-3 mt-3 text-green-400 fill-current">
+              check_circle
+            </span>
+            Business current account
+          </SectionInfoTitle>
           <SectionInfoDescription>
             Free business bank accounts built specifically for small businesses-
             All inclusive; invoicing, virtual cards, store management tools with
             no hidden charges.
           </SectionInfoDescription>
-          <SectionInfoTitle>Financing</SectionInfoTitle>
+          <SectionInfoTitle>
+            <span class="material-icons md-18 mr-3 mt-3 text-green-400 fill-current">
+              check_circle
+            </span>
+            Financing
+          </SectionInfoTitle>
           <SectionInfoDescription>
             Loans and Overafts to finance and fund your business on rainy days,
             With flexible payment plans.
           </SectionInfoDescription>
-          <SectionInfoTitle>Payments</SectionInfoTitle>
+          <SectionInfoTitle>
+            <span class="material-icons md-18 mr-3 mt-3 text-green-400 fill-current">
+              check_circle
+            </span>
+            Payments
+          </SectionInfoTitle>
           <SectionInfoDescription>
             Go cashless by making and accepting payments via Bank Transfer,
             Cards, Mobile Money, USSD, QR Pay and point-of-sale (POS) at a very
