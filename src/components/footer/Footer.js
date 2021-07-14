476f4106ad561929d3f321829f5413babbe21896
silva <sylvernus@glade.ng>
2021-07-14 15:31:19 +0100

    FIX: footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,13 +9,35 @@ import appStoreImg from "assets/img/footer-app-store.png";
 import googlePlayImg from "assets/img/footer-google-play.png";
 
 function Footer(props) {
+  const footerTopLinks = [
+    {
+      link: "Careers",
+      path: "/",
+    },
+    {
+      link: "Blog",
+      path: "/",
+    },
+    {
+      link: "Brand",
+      path: "/",
+    },
+    {
+      link: "Terms",
+      path: "/",
+    },
+    {
+      link: "Privacy & Policy",
+      path: "/",
+    },
+  ];
   return (
     <div className="Footer flex justify-center bg-primary-light bg-opacity-10 p-8">
-      <div className="container grid gap-5 md:grid-cols-3">
-        <div className="flex">
-          <div>
-            <Logo />
-          </div>
+      <div className="container flex md:flex-row flex-col">
+        <div className="flex-0 p-1">
+          <Logo />
+        </div>
+        <div className="flex-1 p-1">
           <div>
             <p className="Footer__item__text">
               Proudly made for small businesses, startups, and freelancers. Our
@@ -35,10 +57,13 @@ function Footer(props) {
                   <img src={image} alt="facebook" />
                 </a>
               ))}
+              <p className="Footer__item__slack">
+                <a href="/" className="text-primary">Slack Us</a> Here
+              </p>
             </div>
           </div>
         </div>
-        <div>
+        <div className="flex-1 p-1">
           <p className="Footer__item__header">HEAD QUATERS</p>
           <p className="Footer__item__text">
             651 N Broad St, Suite 206 Middletown, 19709 New Castle, Delaware,
@@ -46,20 +71,29 @@ function Footer(props) {
           </p>
           <p className="Footer__item__header">NIGERIA</p>
           <p className="Footer__item__text">
-            651 N Broad St, Suite 206 Middletown, 19709 New Castle, Delaware,
-            US.
+            2 Crown Court Estate 2, Oniru Victoria Island Lagos.
           </p>
           <p className="Footer__item__text">
             No. 15 J.J. Oluleye Street, Utako Abuja.
           </p>
         </div>
-        <div>
+        <div className="flex-1 p-1">
           <p className="Footer__item__header">CERTIFIED</p>
           <p className="Footer__item__text">
             Mastercard Securecode, Verified by VISA, PCI DSS Compliant.
           </p>
+          <p className="Footer__item__toplink">
+            <ul>
+              {footerTopLinks.map((link) => (
+                <li>
+                  <a href={link.path}>{link.link}</a>
+                </li>
+              ))}
+            </ul>
+          </p>
+
           <p className="Footer__item__text">
-            No. 15 J.J. Oluleye Street, Utako Abuja.
+            © Glade 2021. All Rights Reserved
           </p>
           <div className="flex md:flex-row flex-col">
             <img src={appStoreImg} width={100} alt="appstore" className="m-1" />
