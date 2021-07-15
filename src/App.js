ba2055c135510c3e1dd722c2df3f930d3951343b
silva <sylvernus@glade.ng>
2021-07-15 10:39:03 +0100

    FEAT: scroll to top

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import BusinesBankAccount from "./pages/businesBankAccount/businesBankAccount";
+import ScrollToTop from "utils/ScrollToTop";
 
 function App() {
   return (
     <React.Suspense fallback={<div>Loading...</div>}>
       <Router>
-        <Switch>
-          {ROUTES.map((route, key) => (
-            <Route key={key} {...route} />
-          ))}
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            {ROUTES.map((route, key) => (
+              <Route key={key} {...route} />
+            ))}
+          </Switch>
+        </ScrollToTop>
       </Router>
     </React.Suspense>
   );
